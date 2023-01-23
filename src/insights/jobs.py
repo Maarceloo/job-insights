44c35aaf5a8ba1e908b462745d7e61fe72b960c6
Marcelo De Lima <delima_marcelo@outlook.com>
2023-01-23 17:31:47 -0300

    feat function filter_by_job_type inplemented

--- src/insights/jobs.py
+++ src/insights/jobs.py
@@ -3,9 +3,6 @@ from typing import List, Dict
 import csv
 
 
-# PATH = "data/jobs.csv"
-
-
 @lru_cache
 def read(path: str) -> List[Dict]:
     with open(path) as file:
@@ -23,21 +20,8 @@ def get_unique_job_types(path: str) -> List[str]:
 
 
 def filter_by_job_type(jobs: List[Dict], job_type: str) -> List[Dict]:
-    """Filters a list of jobs by job_type
-
-    Parameters
-    ----------
-    jobs : list
-        List of jobs to be filtered
-    job_type : str
-        Job type for the list filter
-
-    Returns
-    -------
-    list
-        List of jobs with provided job_type
-    """
-    raise NotImplementedError
-
-
-# print(get_unique_job_types(PATH))
+    result = []
+    for job in jobs:
+        if job_type == job['job_type']:
+            result.append(job)
+    return result
