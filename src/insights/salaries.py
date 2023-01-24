ce74f617fd6871651723388694110b1c2a810609
Marcelo De Lima <delima_marcelo@outlook.com>
2023-01-24 17:42:45 -0300

    feat: matches_salary_range; filter_by_salary_range

--- src/insights/salaries.py
+++ src/insights/salaries.py
@@ -23,46 +23,27 @@ def get_min_salary(path: str) -> int:
 
 
 def matches_salary_range(job: Dict, salary: Union[int, str]) -> bool:
-    """Checks if a given salary is in the salary range of a given job
-
-    Parameters
-    ----------
-    job : dict
-        The job with `min_salary` and `max_salary` keys
-    salary : int
-        The salary to check if matches with salary range of the job
-
-    Returns
-    -------
-    bool
-        True if the salary is in the salary range of the job, False otherwise
-
-    Raises
-    ------
-    ValueError
-        If `job["min_salary"]` or `job["max_salary"]` doesn't exists
-        If `job["min_salary"]` or `job["max_salary"]` aren't valid integers
-        If `job["min_salary"]` is greather than `job["max_salary"]`
-        If `salary` isn't a valid integer
-    """
-    raise NotImplementedError
+    try:
+        max = int(job["max_salary"])
+        min = int(job["min_salary"])
+        money = int(salary)
+        if min > max:
+            raise ValueError("min_salary > max_salary")
+        return min <= money <= max
+    except (ValueError, TypeError, KeyError):
+        raise ValueError
 
 
 def filter_by_salary_range(
     jobs: List[dict], salary: Union[str, int]
 ) -> List[Dict]:
-    """Filters a list of jobs by salary range
 
-    Parameters
-    ----------
-    jobs : list
-        The jobs to be filtered
-    salary : int
-        The salary to be used as filter
-
-    Returns
-    -------
-    list
-        Jobs whose salary range contains `salary`
-    """
-    raise NotImplementedError
+    newListJobs = []
+    for job in jobs:
+        try:
+            isJob = matches_salary_range(job, salary)
+            if isJob:
+                newListJobs.append(job)
+        except ValueError:
+            continue
+    return newListJobs
