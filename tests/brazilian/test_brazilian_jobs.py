f8ee42c614b570207ccceed650a10330a8646931
Marcelo De Lima <delima_marcelo@outlook.com>
2023-02-02 18:05:22 -0300

    test: test_brazilian_jobs implemented

--- tests/brazilian/test_brazilian_jobs.py
+++ tests/brazilian/test_brazilian_jobs.py
@@ -1,5 +1,10 @@
-# from src.pre_built.brazilian_jobs import read_brazilian_file
+from src.pre_built.brazilian_jobs import read_brazilian_file
 
 
 def test_brazilian_jobs():
-    pass
+    PATH = "tests/mocks/brazilians_jobs.csv"
+    arquivo = read_brazilian_file(PATH)
+    for job in arquivo:
+        assert 'title' in job
+        assert 'salary' in job
+        assert 'type' in job
