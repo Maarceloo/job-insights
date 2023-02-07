53297787bad555b689ffff9e77512d7657e2674d
Marcelo De Lima <delima_marcelo@outlook.com>
2023-02-07 17:52:52 -0300

    fit

--- src/flask_app/routes_and_views.py
+++ src/flask_app/routes_and_views.py
@@ -84,7 +84,7 @@ def list_jobs():
 def job(index):
     jobs = read(path="data/jobs.csv")
     job = get_job(jobs, index)
-    return render_template("job.jinja2", job)
+    return render_template("job.jinja2", job=job)
 
 
 def init_app(app: Flask):
